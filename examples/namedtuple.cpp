50b5251d40d07e89de6b4b9050529b393ea8660d
Moritz Kiehn <msmk@cern.ch>
2018-02-16 17:49:53 +0100

    add namedtuple example

---
+++ examples/namedtuple.cpp
@@ -0,0 +1,40 @@
+#include <cstdlib>
+#include <iostream>
+
+#include <dfe_namedtuple.hpp>
+
+struct Data {
+  uint32_t dac0;
+  uint32_t temperature;
+  uint32_t timestamp;
+  float humidity;
+
+  DFE_NAMEDTUPLE(Data, dac0, temperature, timestamp, humidity);
+};
+
+int
+main(int argc, char* argv[])
+{
+  dfe::CsvNamedtupleWriter<Data> csv("test.csv");
+  dfe::NpyNamedtupleWriter<Data> npy("test.npy");
+
+  Data x;
+  x.dac0 = 0;
+  x.temperature = 18289;
+  x.timestamp = 1012;
+  x.humidity = 0.012;
+
+  for (const auto& name : x.names()) {
+    std::cout << name << std::endl;
+  }
+
+  for (uint32_t i = 0; i < 1024; ++i) {
+    x.dac0 = i;
+
+    std::cout << x << std::endl;
+    csv.append(x);
+    npy.append(x);
+  }
+
+  return EXIT_SUCCESS;
+}
